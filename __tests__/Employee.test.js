4580bacbdd34e8e61a9c39f214ca696d89f8b8a8
Ryely <ryely.moore@outlook.com>
2021-07-02 03:39:53 -0600

    created tests for the required methods, confirmed failure, wrote required methods, checked success

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,11 +1,26 @@
 const { test, expect } = require('@jest/globals');
 const Employee = require('../lib/Employee');
-
+// creates an initial Employee object to test with
+const emp = new Employee('Rye',1,'sample@gmail.com');
 // test that the properties of the employee object are being created properly
 test('creates an employee object',() => {
-    const emp = new Employee('Rye',1,'sample@gmail.com');
-
     expect(emp.name).toBe('Rye');
     expect(emp.id).toEqual(1);
     expect(emp.email).toBe('sample@gmail.com');
+})
+
+test('getName() returns the created name', () => {
+    expect(emp.getName()).toBe('Rye');
+})
+
+test('getId() returns the created id', () => {
+    expect(emp.getId()).toEqual(1);
+})
+
+test('getEmail() returns the created email', () => {
+    expect(emp.getEmail()).toBe('sample@gmail.com');
+})
+
+test('getRole() returns the role "Employee"', () => {
+    expect(emp.getRole()).toBe('Employee');
 })
