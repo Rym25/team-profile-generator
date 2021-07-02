6a5ef9a31b15a015f2034ae3f456a388329c8aa0
Ryely <ryely.moore@outlook.com>
2021-07-02 04:30:05 -0600

    wrote test for Engineer method getGitHub(), failed

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,3 +1,4 @@
+const { test, expect } = require('@jest/globals');
 const Engineer = require('../lib/Engineer');
 // creates data to feed into Engineer class
 const data = {name:'Rye',id:1,email:'sample@gmail.com',gitUserName:'Rym25'};
@@ -9,4 +10,14 @@ test('creates a eng object with all of the required parameters', () => {
     expect(eng.id).toEqual(1);
     expect(eng.email).toBe('sample@gmail.com');
     expect(eng.gitUserName).toBe('Rym25');
+})
+
+test('confirms the inherited methods function', () => {
+    expect(eng.getName()).toBe('Rye');
+    expect(eng.getId()).toEqual(1);
+    expect(eng.getEmail()).toBe('sample@gmail.com');
+})
+
+test('confirms Engineer method returns the GitHub username', () => {
+    expect(eng.getGitHub()).toBe('Rym25');
 })
