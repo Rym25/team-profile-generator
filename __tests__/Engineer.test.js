e52cf888565775b592ac7c4ffdf8abcd11fd5201
Ryely <ryely.moore@outlook.com>
2021-07-02 04:26:18 -0600

    wrote Engineer class with required props, tested successfully.

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -4,9 +4,9 @@ const data = {name:'Rye',id:1,email:'sample@gmail.com',gitUserName:'Rym25'};
 // creates new Engineer object to test using the above data
 const eng = new Engineer(data);
 
-test('creates a man object with all of the required parameters', () => {
+test('creates a eng object with all of the required parameters', () => {
     expect(eng.name).toBe('Rye');
     expect(eng.id).toEqual(1);
     expect(eng.email).toBe('sample@gmail.com');
-    expect(eng.gitUserName).toBe('Rym');
+    expect(eng.gitUserName).toBe('Rym25');
 })
